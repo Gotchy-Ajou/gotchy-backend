8e61d55dc6106efd91f08d352dd226fd48b3bb3c
DaWoonKang <nachokang@ajou.ac.kr>
2023-05-16 19:12:02 +0900

    initial commit

---
+++ src/main/java/ajou/se/gotchy/GotchyApplication.java
@@ -0,0 +1,13 @@
+package ajou.se.gotchy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotchyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotchyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ajou/se/gotchy/GotchyApplicationTests.java
@@ -0,0 +1,13 @@
+package ajou.se.gotchy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotchyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

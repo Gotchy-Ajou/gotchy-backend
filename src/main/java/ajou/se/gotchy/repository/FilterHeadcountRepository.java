2e498c9d08548d6d963a7f9b7a15da20b3712d4b
dajin jeong <da8411@ajou.ac.kr>
2023-06-03 21:06:19 +0900

    Delete file

--- src/main/java/ajou/se/gotchy/repository/FilterHeadcountRepository.java
+++
@@ -1,7 +0,0 @@
-package ajou.se.gotchy.repository;
-
-import ajou.se.gotchy.domain.entity.Gotchy;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FilterHeadcountRepository extends JpaRepository<Gotchy, Long> {
-}
